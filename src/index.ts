5af6da21143c11fa2eb9d1230725967d55d58c24
jianrong7 <ipwnjianrong@gmail.com>
2023-09-13 16:48:56 +0800

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,15 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .get("/", () => "Hello Elysia")
+  .get("/ping", () => ({ message: "pong" }))
+  .get("/morecomplicatedping", () => {
+    const hello = "hello";
+    return {
+      message: `${hello} pong`,
+    };
+  })
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
